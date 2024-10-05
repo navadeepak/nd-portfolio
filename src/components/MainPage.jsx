db223939e0a7bf745e442257d216c8a2475aff26
navadeepak <navadeepakaswin007@gmail.com>
2024-10-05 10:33:35 +0530

    MUI imported

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 export default function MainPage() {
   return (
     <div>
-      hello
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Avatar from '@mui/material/Avatar';
+
+export default function Navbar() {
+  return (
+    <div className='w-full h-[8vh] bg-zinc-200'>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+    </div>
+  )
+}
